8bcadc8b4a95a60c0bb1d3a65b4dd6e20be17ae8
ChristianMrCoder <siboruremarukundoc@gmail.com>
2023-04-21 09:51:20 +0200

    commit

--- api/routes/User.js
+++ api/routes/User.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const VerifyToken = require('./verifyToken')
 
-router.get('/', (req, res) => {
-    res.status(200).json("User Routes")
+router.put('/:id', (req, res) => {
+    if (req.body._id === req.params.id) {
+        if (req.body.password) {
+            
+        }
+    } else {
+        res.status(403).json("You can update only your account")
+    }
 })
 
 
--- api/routes/verifyToken.js
+++ api/routes/verifyToken.js
@@ -1,13 +1,16 @@
 const jwt = require('jsonwebtoken');
 
-export function VerifyToken(req, res, next) {
+ function VerifyToken(req, res, next) {
     const authHeaders = req.headers;
     if (authHeaders) {
         const token = authHeaders.split(" ")[1];
         jwt.verify(token, process.env.SECRET, (error, user) => {
-            if (error) throw error;
+            if (error) res.status(401).json("The token is Invalid");
+            req.user = user
+            next();
         })
     } else {
         res.status(200).json("UnAuthnticated User");
     }
 }
+module.exports = VerifyToken
